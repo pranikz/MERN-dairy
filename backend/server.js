19e472f05518d0e47dd4c33aadf15ccc7027cba7
Pratyush Mahapatra <pratyushmahapatra2000@gmail.com>
2022-01-29 22:39:49 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -8,22 +8,29 @@ const port = process.env.PORT || 5000;
 const userRoutes = require("./routes/userRoutes");
 const noteRoutes = require("./routes/noteRoutes");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
+const path  = require("path");
+const res = require("express/lib/response");
 connectDB();
 app.use(express.json());
 
-app.get("/", (req, res) => res.send("Hello World! API working"));
-
-// app.get("/api/notes", (req, res) => {
-//   res.json(notes);
-// });
-
-// app.get("/api/notes/:id", (req, res) => {
-//   const note = notes.find((n) => n._id === req.params.id);
-//   res.send(note);
-// });
 
 app.use("/api/users", userRoutes);
 app.use("/api/notes", noteRoutes);
+
+//deployments
+
+__dirname=path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build/")));
+  app.get("*",()=> {
+    res.sendFile(path.resolve(__dirname , 'frontend','build', 'index.html'));
+  });
+} else {
+  app.get("/", (req, res) => res.send("Hello World! API working"));
+}
+
+// deployment
+
 app.use(notFound);
 app.use(errorHandler);
 
