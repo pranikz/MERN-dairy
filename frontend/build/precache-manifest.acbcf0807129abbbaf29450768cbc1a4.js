e864700105832263a74cb0182c88bb05d6f7a430
Pratyush Mahapatra <pratyushmahapatra2000@gmail.com>
2022-01-29 22:46:19 +0530

    deployment

---
+++ frontend/build/precache-manifest.acbcf0807129abbbaf29450768cbc1a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27a0116420ed91aa8b11bdb222c4f911",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0e526723ef1c7697e15",
+    "url": "/static/css/main.06110d58.chunk.css"
+  },
+  {
+    "revision": "09a3a5c457f7fb28147c",
+    "url": "/static/js/2.29fb53e6.chunk.js"
+  },
+  {
+    "revision": "dbf274aca0f59e62f144e5feb7ddbbe2",
+    "url": "/static/js/2.29fb53e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0e526723ef1c7697e15",
+    "url": "/static/js/main.1339e80a.chunk.js"
+  },
+  {
+    "revision": "6613802de99d406509fd",
+    "url": "/static/js/runtime-main.8d5285fa.js"
+  }
+]);
